1cdd1f367bd553f6060731c8d234c93f393f05d9
Amiel Nava <amieln@icloud.com>
2025-03-27 11:24:01 -0700

    Updated link tokens for absolute paths

--- src/app/routes/router-tokens.model.ts
+++ src/app/routes/router-tokens.model.ts
@@ -1,7 +1,13 @@
 export enum ROUTER_TOKENS {
   HOME = "",
+  MENU = "menu",
+  SIGN_IN = "auth/sign-in",
+  SIGN_UP = "auth/sign-up",
 }
 
 export enum LINK_TOKENS {
   HOME = `/${ROUTER_TOKENS.HOME}`,
+  MENU = `/${ROUTER_TOKENS.MENU}`,
+  SIGN_IN = `/${ROUTER_TOKENS.SIGN_IN}`,
+  SIGN_UP = `/${ROUTER_TOKENS.SIGN_UP}`,
 }
